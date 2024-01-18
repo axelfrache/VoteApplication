2f55cd9b1e8decd6c731db7faa52d5234003b01a
frachea <axel.frache@etu.umontpellier.fr>
2024-01-18 11:38:04 +0100

    Fonctionnalité pour vérifier la présence d'un électeur

--- code/src/serveur_vote.c
+++ code/src/serveur_vote.c
@@ -100,8 +100,30 @@ void traitementSupprimeElecteur(SupprimeElecteurCmd *cmd) {
 
 void traitementEstPresent(EstPresentCmd *cmd) {
     printf("Traitement EstPresentCmd\n");
-    // Implémentation spécifique pour EstPresentCmd
+    if (cmd == NULL || cmd->identifiant[0] == '\0') {
+        printf("Commande invalide ou identifiant manquant.\n");
+        return;
+    }
+
+    // Ouvrir la base de données
+    sqlite3 *db;
+    if (sqlite3_open("./../data_base/base_de_donnees.db", &db) != SQLITE_OK) {
+        fprintf(stderr, "Erreur lors de l'ouverture de la base de données: %s\n", sqlite3_errmsg(db));
+        return;
+    }
+
+    // Vérifier si l'électeur existe
+    int exists = electeurExists(db, cmd->identifiant, ENTITY_ID_SIZE);
+    if (exists) {
+        printf("L'électeur est présent dans la base de données.\n");
+    } else {
+        printf("L'électeur n'est pas présent dans la base de données.\n");
+    }
+
+    // Fermer la base de données
+    sqlite3_close(db);
 }
+
 // Thread pour le traitement des commandes
 void* processCommands(void* arg) {
     while (1) {

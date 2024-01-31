0a40e88d5076318a7636b1e95dc4607af7768030
gaeta <gaetan.gonfiantini@gmail.com>
2024-01-31 21:49:12 +0100

    Ajout des dernière captures d'écran

--- code/test/test_Fonctions_BD/test_election.c
+++ code/test/test_Fonctions_BD/test_election.c
@@ -14,17 +14,14 @@ int main(int argc, char *argv[])
     GError *error = NULL;
     GOptionContext *context;
 
-    // Définition des options
     gboolean verbose = FALSE;
     GOptionEntry entries[] = {
             {"verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose, "Mode verbeux", NULL},
             {NULL}};
 
-    // Création d'un contexte d'option
     context = g_option_context_new("- test pour la création de la base de donnée");
     g_option_context_add_main_entries(context, entries, NULL);
 
-    // Analyse des options de ligne de commande
     if (!g_option_context_parse(context, &argc, &argv, &error))
     {
         g_print("Erreur lors de l'analyse des options: %s\n", error->message);
@@ -33,24 +30,20 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Affichage des résultats
     if (verbose)
     {
         g_print("Mode verbeux activé\n");
     }
 
-    // Initialisation de la base de données
     const char *dbPath = "../data_base/base_de_donnees.db";
     sqlite3 *db;
 
-    // Ouvrir ou créer la base de données si elle n'existe pas
     db = database_open(dbPath);
     if (!db) {
         fprintf(stderr, "Impossible d'ouvrir ou de créer la base de données.\n");
         return 1;
     }
 
-    // Vider la base de données et l'initialiser
     if (clear_database(db) != 0) {
         fprintf(stderr, "Erreur lors du vidage de la base de données.\n");
         sqlite3_close(db);
@@ -63,17 +56,18 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Début des tests pour les élections
+    // Test: Création d'une élection
     char id[ID_SIZE] = "EXXXXXXXXX";
     const char *question = "This is a binary question";
     createElection(db, id, ID_SIZE, question, "01/01/2023", "02/01/2023", "active");
+    printf("Élection créée avec succès.\n");
+
+    // Test: Vérification de l'existence de l'élection
     int localid = Election_getIdFromNumeroID(db, id, ID_SIZE);
     g_assert(localid == 1);
+    printf("L'élection existe et son ID local est correct (ID attendu: 1, ID obtenu: %d).\n", localid);
 
-    // Fermeture de la base de données
     sqlite3_close(db);
-
-    // Libération de la mémoire
     g_option_context_free(context);
     return 0;
 }
